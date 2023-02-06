6756d1902960444f77318e26d8cf1f6fa9d69857
sunxyw <git@sunxyw.xyz>
2023-02-06 12:24:12 +0800

    Add admin create command

---
+++ app/Console/Commands/CreateAdminCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+
+class CreateAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an admin user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $name = $this->ask('What is your name?');
+        $email = $this->ask('What is your email?');
+        $password = $this->ask('What is your password?');
+
+        $user = app(CreatesNewUsers::class)->create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password,
+            'terms' => true,
+        ]);
+
+        return self::SUCCESS;
+    }
+}
