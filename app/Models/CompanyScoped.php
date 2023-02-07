1769f545428c708ed1390e2559dc4ed419dcb616
sunxyw <git@sunxyw.xyz>
2023-02-07 22:21:56 +0800

    Fix company scope when not logged in

--- app/Models/CompanyScoped.php
+++ app/Models/CompanyScoped.php
@@ -6,14 +6,16 @@ trait CompanyScoped
 {
     protected static function bootCompanyScoped()
     {
-        static::addGlobalScope('company', function ($builder) {
-            $builder->where('company_id', auth()->user()->currentCompany->id);
-        });
+        if (auth()->check() && auth()->user()->currentCompany) {
+            static::addGlobalScope('company', function ($builder) {
+                $builder->where('company_id', auth()->user()->currentCompany->id);
+            });
 
-        static::creating(function ($model) {
-            if (!isset($model->company_id)) {
-                $model->company_id = auth()->user()->currentCompany->id;
-            }
-        });
+            static::creating(function ($model) {
+                if (!isset($model->company_id)) {
+                    $model->company_id = auth()->user()->currentCompany->id;
+                }
+            });
+        }
     }
 }
