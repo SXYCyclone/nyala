eeef5dd90914b01e61819e473c715a07bbae1e46
sunxyw <git@sunxyw.xyz>
2023-02-09 22:02:59 +0800

    Add admin view indicator

--- app/Providers/FilamentAdminServiceProvider.php
+++ app/Providers/FilamentAdminServiceProvider.php
@@ -3,8 +3,20 @@
 namespace App\Providers;
 
 use Artificertech\FilamentMultiContext\ContextServiceProvider;
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\View;
 
 class FilamentAdminServiceProvider extends ContextServiceProvider
 {
     public static string $name = 'filament-admin';
+
+    public function bootingPackage()
+    {
+        parent::bootingPackage();
+
+        Filament::registerRenderHook('global-search.start', fn() => View::make('filament-environment-indicator::badge', [
+            'color' => '#DC0504',
+            'environment' => 'Admin',
+        ]));
+    }
 }
